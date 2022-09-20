56121c28054f53d96f81ac50eac4a2b82de33ee2
Sahil Sharma <74351253+ys1113457623@users.noreply.github.com>
2022-09-20 16:20:58 +0530

    source deleted

--- lib/models/article.dart
+++ lib/models/article.dart
@@ -14,23 +14,3 @@ class Article{
 
 
 }
-
-class Source {
-  String id;
-  String name;
-
-  Source({
-    this.id,
-    this.name,
-  });
-
-  factory Source.fromJson(Map<String, dynamic> json) => Source(
-    id: json["id"] == null ? null : json["id"],
-    name: json["name"],
-  );
-
-  Map<String, dynamic> toJson() => {
-    "id": id == null ? null : id,
-    "name": name,
-  };
-}
