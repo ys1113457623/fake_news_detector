56caf6097c8c7f80c75ecccbb5a92e91d05a3c35
Sahil Sharma <74351253+ys1113457623@users.noreply.github.com>
2022-10-09 21:21:32 +0530

    fast api implemented

--- API/app.py
+++ API/app.py
@@ -1,17 +1,28 @@
-from flask import Flask, render_template, request
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 21:40:41 2020
+@author: win10
+"""
+
+# 1. Library imports
+from sklearn.model_selection import train_test_split
+import uvicorn
+from fastapi import FastAPI
+from typing import Union
+
+from fastapi import FastAPI, Query
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import PassiveAggressiveClassifier
 import pickle
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
-app = Flask(__name__)
+# 2. Create the app object
+app = FastAPI()
 tfvect = TfidfVectorizer(stop_words='english', max_df=0.7)
-loaded_model = pickle.load(open('model.pkl', 'rb'))
+loaded_model = pickle.load(open('API/model.pkl', 'rb'))
 
-# We need to fit the TFIDF VEctorizer
-fake_df = pd.read_csv('Fake.csv')
-real_df = pd.read_csv('True.csv')
+
+fake_df = pd.read_csv('API/Fake.csv')
+real_df = pd.read_csv('API/True.csv')
 fake_df.drop(['date', 'subject'], axis=1, inplace=True)
 real_df.drop(['date', 'subject'], axis=1, inplace=True)
 
@@ -38,26 +49,43 @@ def fake_news_det(news):
     input_data = [news]
     vectorized_input_data = tfvect.transform(input_data)
     prediction = loaded_model.predict(vectorized_input_data)
+    # print(classification_report(y_test,prediction))
+    # return precision_score(y_test,prediction)
     return prediction
 
-# Defining the site route
+# 3. Index route, opens automatically on http://127.0.0.1:8000
+@app.get('/')
+def index():
+    return {'message': 'Hello, World'}
 
+# 4. Route with a single parameter, returns the parameter within a message
+#    Located at: http://127.0.0.1:8000/AnyNameHere
+# @app.get('/{name}')
+# def get_name(name: str):
+#     return {'Welcome To Krish Youtube Channel': f'{name}'}
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# 3. Expose the prediction functionality, make a prediction from the passed
+#    JSON data and return the predicted Bank Note with the confidence
+# @app.get('/predict')
+# def predict():
+#     message = request
+#     if request.method == 'GET':
+#         message = request.args['query']
+#         pred = fake_news_det(message)
+#         print(pred)
+#         return str(pred)
+#     else:
+#         return render_template('index.html', prediction="Something went wrong")
 
+@app.get('/{predict}')
+def get_name(predict: str):
+    pred = fake_news_det(predict)
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    if request.method == 'GET':
-        message = request.args['query']
-        pred = fake_news_det(message)
-        print(pred)
-        return str(pred)
-    else:
-        return render_template('index.html', prediction="Something went wrong")
+    return str(pred)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+# 5. Run the API with uvicorn
+#    Will run on http://127.0.0.1:8000
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
+    
+#uvicorn app:app --reload
